ac661125355e39f000c29069293251c458768e65
Ronald (Jeff) Lopez <32562609+lopezjronald@users.noreply.github.com>
2021-06-21 14:57:27 -0700

    initial commit

---
+++ src/main/java/com/asts/clothinginventory/ClothingInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.asts.clothinginventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothingInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothingInventoryApplication.class, args);
+    }
+
+}
